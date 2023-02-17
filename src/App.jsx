17da4e305f7be61bc0acd365d92b513acef15c0e
Vamsi <vamsitupakula474@gmail.com>
2023-02-17 23:39:44 +0530

    project done...

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import MoviePane from "./components/MoviePane";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [input, setInput] = useState("Tenet");
+  const [movie, setMovie] = useState(null);
+
+  const changeInput = (e) => {
+    setInput(e.target.value);
+  }
+
+  const changeMovie = () => {
+    axios.get(`http://www.omdbapi.com/?apikey=56546cdc&t=${input}`)
+      .then(res => {
+        if (res.data.Response === "True") {
+          setMovie(res.data);
+          setInput("");
+        } else {
+          window.alert("Movie not found");
+        }
+      })
+  }
+
+  useEffect(() => {
+    axios.get(`http://www.omdbapi.com/?apikey=56546cdc&t=tenet`).then(res => setMovie(res.data));
+    console.log(movie)
+  }, [])
+
+  return (
+      <div className="container">
+        <h1 className="websiteName">Movies DataBase - Search Movie Get Details</h1>
+        <div className="searchBar">
+          <input type="text" className="searchInput" onChange={changeInput} placeholder="Type to search....." spellCheck='false'/>
+          <button className="searchBtn" onClick={changeMovie}>Search</button>
+        </div>
+        {movie && <MoviePane movie={movie} />}
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/MoviePane.jsx
@@ -0,0 +1,23 @@
+import './MoviePane.css'
+
+function MoviePane(props) {
+    return ( 
+        <div className="movieDetails">
+            <img src={props.movie.Poster} alt="img text" id='poster'/>
+            <div className="details">
+                    <div className="title head">Title</div>
+                    <div className="title value">: {props.movie.Title}</div>
+                    <div className="writer head">Writer</div>
+                    <div className="writer value">: {props.movie.Writer}</div>
+                    <div className="director head">Director</div>
+                    <div className="director value">: {props.movie.Director}</div>
+                    <div className="imdb_rating head">IMDB Rating</div>
+                    <div className="imdb_rating value">: {props.movie.imdbRating}</div>
+                    <div className="year head">Year</div>
+                    <div className="year value">: {props.movie.Year}</div>
+            </div>
+        </div>
+     );
+}
+
+export default MoviePane;
