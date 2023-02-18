c4436e085b1fa3c897d537868129c9c75d9fb16c
Vamsi <vamsitupakula474@gmail.com>
2023-02-18 09:17:49 +0530

    app.jsx modified

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
           <input type="text" className="searchInput" onChange={changeInput} placeholder="Type to search....." spellCheck='false'/>
           <button className="searchBtn" onClick={changeMovie}>Search</button>
         </div>
-        {(movie === null) ? '' : <MoviePane movie={movie} />}
+        <MoviePane movie={movie} />
       </div>
   );
 }
